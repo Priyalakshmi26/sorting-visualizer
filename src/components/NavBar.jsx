27d3006126ba7e83597fc794792e448ca57f4025
Priyalakshmi P.R <84332515+Priyalakshmi26@users.noreply.github.com>
2022-11-11 09:46:04 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -41,7 +41,7 @@ export function NavBar() {
         }}
       >
         <h3>Sorting Algorithms Visualizer</h3>
-        <a href="https://github.com/Priyalakshmi26/sorting-visualiser" target="_blank"><AiFillGithub style={{ fontSize: "1.5rem" }} /></a>
+        <a href="https://github.com/Priyalakshmi26/sorting-visualizer" target="_blank"><AiFillGithub style={{ fontSize: "1.5rem" }} /></a>
       </div>
       <AppBar position="static" color="default">
         <Tabs
